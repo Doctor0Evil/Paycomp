56b2adcae572a7a5aee770d17a5a4a4614f3ca68
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-21 12:35:28 -0700

    Update lib.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

--- cyconetics-decision-grammar/src/lib.rs
+++ cyconetics-decision-grammar/src/lib.rs
@@ -3,8 +3,10 @@ pub mod roles;
 pub mod roh_guard;
 pub mod ledger;
 pub mod macros;
+pub mod evolution_neuroassist;
 
 pub use types::*;
 pub use roles::*;
 pub use roh_guard::*;
 pub use ledger::*;
+pub use evolution_neuroassist::NeuroAssistV2Graph;
